3e34ea70d77d2f3dc2d9e0e59036d5b17be9e0cf
Andy Green <andy@ancientcoder.com>
2022-11-13 16:53:41 +1000

    Improved code to limit input window to the LayoutBuilder content Renamed members so they make more sense for what the demo is showing

--- Examples_MouseDetect/lib/main.dart
+++ Examples_MouseDetect/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'polygon_test.dart';
 import 'dart:developer' as debug;
 
 void main() {
@@ -14,36 +13,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Polygon Demo',
+      title: 'User Tap/Mouse Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Polygon Render Test'),
+      home: const MyHomePage(title: 'User Tap/Mouse Input Test'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -51,98 +31,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  int _enterCounter = 0;
-  int _exitCounter = 0;
   double x = 0.0;
   double y = 0.0;
-  int _downCounter = 0;
-  int _upCounter = 0;
-
-  void _incrementEnter(PointerEvent details) {
-    setState(() {
-      _enterCounter++;
-      debug.log("Enter: $_enterCounter");
-    });
+  void _inputEnter(PointerEvent details) {
+    debug.log("Enter");
   }
 
-  void _incrementExit(PointerEvent details) {
-    setState(() {
-      _exitCounter++;
-      debug.log("Exit $_exitCounter");
-    });
+  void _inputExit(PointerEvent details) {
+    debug.log("Exit");
   }
 
-  void _updatePosition(PointerEvent details) {
-    setState(() {
-      x = details.position.dx;
-      y = details.position.dy;
-      debug.log("Pos X: $x, Y: $y");
-    });
+  void _inputPositionChange(PointerEvent details) {
+    x = details.position.dx;
+    y = details.position.dy;
+    debug.log("Pos X: $x, Y: $y");
   }
 
-  void _incrementPressed(PointerEvent details) {
-    _updatePosition(details);
-    setState(() {
-      _downCounter++;
-      debug.log("Pressed");
-    });
+  void _inputPressed(PointerEvent details) {
+    _inputPositionChange(details);
+    debug.log("Pressed");
   }
 
-  void _incrementReleased(PointerEvent details) {
-    _updatePosition(details);
-    setState(() {
-      _upCounter++;
-      debug.log("Released");
-    });
+  void _inputReleased(PointerEvent details) {
+    _inputPositionChange(details);
+    debug.log("Released");
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Listener(
-        onPointerDown: _incrementPressed,
-        onPointerMove: _updatePosition,
-        onPointerUp: _incrementReleased,
-        child: MouseRegion(
-          onEnter: _incrementEnter,
-          onHover: _updatePosition,
-          onExit: _incrementExit,
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            color: Colors.blueAccent,
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        color: Colors.blueAccent,
+        child: Listener(
+          onPointerDown: _inputPressed,
+          onPointerMove: _inputPositionChange,
+          onPointerUp: _inputReleased,
+          child: MouseRegion(
+            onEnter: _inputEnter,
+            onHover: _inputPositionChange,
+            onExit: _inputExit,
             child: LayoutBuilder(
               builder: (_, constraints) => Container(
                 width: constraints.widthConstraints().maxWidth,
                 height: constraints.heightConstraints().maxHeight,
                 color: Colors.blueGrey,
-                child: CustomPaint(
-                    painter: PolygonTest(
-                        constraints.widthConstraints().maxWidth,
-                        constraints.heightConstraints().maxHeight)),
               ),
             ),
           ),
